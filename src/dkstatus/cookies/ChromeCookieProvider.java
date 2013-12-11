50cbb5120a129673fab61958f7d13ceb3ee9418b
Eclipse <novotny.jan23@gmail.com>
2013-12-11 18:57:21 +0100

    fungujici nacitani ve chromu

--- src/dkstatus/cookies/ChromeCookieProvider.java
+++ src/dkstatus/cookies/ChromeCookieProvider.java
@@ -2,10 +2,14 @@
 package dkstatus.cookies;
 
 import dkstatus.Config;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static java.nio.file.StandardCopyOption.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
@@ -26,22 +30,36 @@ public class ChromeCookieProvider implements ICookieProvider {
         CookieStore cookieStore = new BasicCookieStore();
 
         String repositoryPath = System.getenv("LOCALAPPDATA") + "\\Google\\Chrome\\User Data\\Default\\cookies";
+        try {
+            Files.copy(Paths.get(repositoryPath), Paths.get(repositoryPath + ".copy"), REPLACE_EXISTING);
+        } catch (IOException ex) {
+            Logger.getLogger(ChromeCookieProvider.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         try {  
             Class.forName("org.sqlite.JDBC");  
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + repositoryPath);  
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + repositoryPath + ".copy");  
             Statement statement = connection.createStatement();  
             ResultSet resultSet = statement.executeQuery("SELECT * FROM cookies WHERE host_key LIKE '%" + Config.ROOT_DOMAIN + "%'");  
 
             while (resultSet.next()) {  
-                String key = resultSet.getString("host_key");  
-                ResultSetMetaData meta = resultSet.getMetaData();
-                System.out.println(key +" Id: " + resultSet.getString("name") + "\n");
+                String host = resultSet.getString("host_key");  
+                String name = resultSet.getString("name");
+                String value = resultSet.getString("value");
                 
-                BasicClientCookie cookie = new BasicClientCookie(resultSet.getString("name"), resultSet.getString("value"));
-                //cookie.setVersion(0);
-                cookie.setDomain(resultSet.getString("host_key"));
+                //System.out.println(host +" Name: " + name + " Value: " + value);
+                
+                /*if (!name.equals("sid") && !name.equals("cid") && !name.equals("user") && !name.equals("password"))
+                    continue;*/
+                
+                System.out.println(host +" Name: " + name + " Value: " + value);
+                
+                BasicClientCookie cookie = new BasicClientCookie(name, value);
+                cookie.setDomain(host);
                 cookie.setPath(resultSet.getString("path"));
-                cookie.setExpiryDate(resultSet.getDate("expires_utc"));
+                /*String date = resultSet.getString("expires_utc");
+                if (!date.equals("0"))
+                    cookie.setExpiryDate(new java.util.Date(Long.parseLong(date)*1000l));*/
                 cookie.setSecure(resultSet.getBoolean("secure"));
                 
                 cookieStore.addCookie(cookie);                
@@ -49,8 +67,6 @@ public class ChromeCookieProvider implements ICookieProvider {
         } catch(ClassNotFoundException | SQLException e) {
             Logger.getLogger(ChromeCookieProvider.class.toString()).log(Level.SEVERE, "error loading cookies", e);
         } 
-
-
         
         return cookieStore;
     }
--- src/dkstatus/requests/AnnouncesRequest.java
+++ src/dkstatus/requests/AnnouncesRequest.java
@@ -1,6 +1,16 @@
 package dkstatus.requests;
 
+import dkstatus.Utils;
+import dkstatus.cookies.CookieStoreManager;
 import java.io.IOException;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 /**
  *
@@ -10,6 +20,21 @@ public class AnnouncesRequest {
     
     public static String getResult() throws IOException {
         
-        return "";
+        RequestConfig defaultRequestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.BROWSER_COMPATIBILITY).build();
+
+        try (CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCookieStore(CookieStoreManager.getStore())
+                .setDefaultRequestConfig(defaultRequestConfig)
+                .build()) {
+            
+            HttpGet request = new HttpGet(Utils.getRootLink() + "game.php?village=28262&screen=report");
+            request.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");
+
+            System.out.println("executing request " + request.getURI());
+            try (CloseableHttpResponse response = httpclient.execute(request)) {
+                HttpEntity entity = response.getEntity();
+                return EntityUtils.toString(entity);
+            }
+        }
     }
 }
