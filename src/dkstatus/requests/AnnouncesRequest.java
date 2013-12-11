a11f73075bb2d00e390a450d8e83cf12c2ccda16
Eclipse <novotny.jan23@gmail.com>
2013-12-11 17:25:03 +0100

    fixed build

---
+++ src/dkstatus/requests/AnnouncesRequest.java
@@ -0,0 +1,15 @@
+package dkstatus.requests;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Johny
+ */
+public class AnnouncesRequest {
+    
+    public static String getResult() throws IOException {
+        
+        return "";
+    }
+}
