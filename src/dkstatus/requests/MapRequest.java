c80bb113c4d53c784e17eb614222ed91dc8cb6f7
Eclipse <novotny.jan23@gmail.com>
2014-01-16 18:56:23 +0100

    check for both formats of map village data

--- src/dkstatus/requests/MapRequest.java
+++ src/dkstatus/requests/MapRequest.java
@@ -13,6 +13,7 @@ import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -98,7 +99,7 @@ public class MapRequest extends AbstractUpdateRequest {
                     "konec ochrany" nebo 0
                 ],* 
              */
-            JSONArray villages = continent.getJSONArray("villages");
+            Object villages = continent.get("villages");
             //System.out.println(villages.toString(4));
             /*          
                 {
@@ -111,7 +112,8 @@ public class MapRequest extends AbstractUpdateRequest {
                         "100"
                     ],
                  }
-                 {
+            
+                 "x souradnice (plus baseX)": { nebo { (zalezi jestli jsou data strukturovana jako array nebo mapa)
                     "y souradnice (plus baseY)": [
                         "id vesnice",
                         9 nezname, nejaka konstanta,
@@ -129,36 +131,48 @@ public class MapRequest extends AbstractUpdateRequest {
                     ],
                  }            
              */
-            for (int x = 0; x < villages.length(); ++x) {
-                JSONObject villagesX = villages.getJSONObject(x);
-                Iterator<String> it2 = villagesX.keys();
-                while (it2.hasNext()) {
-                    String key2 = it2.next();
-                    int y = Integer.parseInt(key2);
-                    JSONArray village = villagesX.getJSONArray(key2);
-                    
-                    int id = village.getInt(0);
-                    Object n = village.get(2);
-                    String name;
-                    if (n instanceof Integer)
-                        name = "Barbarská vesnice";
-                    else
-                        name = (String)n;
-                    Player owner = new Player();
-                    owner.setId(village.getInt(4));
-                    Village vil = new Village(id, name, owner, new MapPosition(x + baseX, y + baseY));
-                    String points = Utils.clearNumber(village.getString(3));
-                    if (points.equals(""))
-                        vil.setPoints(0);
-                    else
-                        vil.setPoints(Integer.parseInt(points));
-                    world.addVillage(vil);
-                }                
+            // there are two possible structures: array and map - lets check them both
+            if (villages instanceof JSONArray) {
+                for (int x = 0; x < ((JSONArray)villages).length(); ++x)
+                    parseVillageData(((JSONArray)villages).getJSONObject(x), x, baseX, baseY, world);                
+            } else {
+                Iterator<String> it1 = ((JSONObject)villages).keys();
+                while (it1.hasNext()) {
+                    String key = it1.next();
+                    int x = Integer.parseInt(key);
+                    parseVillageData(((JSONObject)villages).getJSONObject(key), x, baseX, baseY, world);                
+                }
             }
         }
         
         WindowManager.getWindow().updateRaidHelpers(world);
         WebRequestService.scheduleTask(new MapRequest(v), Utils.randSec(60 * 60 * 2, 60 * 60 * 3)); // 2 - 3 hours
+    }
+
+    private void parseVillageData(JSONObject villagesX, int x, int baseX, int baseY, World world) throws NumberFormatException, JSONException {
+        Iterator<String> it2 = villagesX.keys();
+        while (it2.hasNext()) {
+            String key2 = it2.next();
+            int y = Integer.parseInt(key2);
+            JSONArray village = villagesX.getJSONArray(key2);
+            
+            int id = village.getInt(0);
+            Object n = village.get(2);
+            String name;
+            if (n instanceof Integer)
+                name = "Barbarská vesnice";
+            else
+                name = (String)n;
+            Player owner = new Player();
+            owner.setId(village.getInt(4));
+            Village vil = new Village(id, name, owner, new MapPosition(x + baseX, y + baseY));
+            String points = Utils.clearNumber(village.getString(3));
+            if (points.equals(""))
+                vil.setPoints(0);
+            else
+                vil.setPoints(Integer.parseInt(points));
+            world.addVillage(vil);
+        }
     }
 
     @Override
